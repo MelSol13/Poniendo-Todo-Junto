3c076eef4bb7205765541dd3f6a787ce171a4fd8
Melissa Solano <m.solanof13@gmail.com>
2023-08-03 19:32:44 -0600

    Ejercicio Completo

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import PersonCard from "./componentes/PersonCard";
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <PersonCard firstName="Elena"
+      lastName="De Troya" age={18}
+      hairColor="Black"/>
+
+      <PersonCard firstName="Juana"
+      lastName="De Arco" age={20}
+      hairColor="Brown"/>
+
+      <PersonCard firstName="Pedro"
+      lastName="Paramo" age={32}
+      hairColor="Blonde"/>
+
+      <PersonCard firstName="Pablo"
+      lastName="Picasso" age={50}
+      hairColor="White"/>
+    </div>
+  );
+}
+
+export default App;
